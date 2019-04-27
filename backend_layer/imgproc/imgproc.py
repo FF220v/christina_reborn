e027604e650e6ba9a1e0f034831a71207baf50ac
FF220v <San987873@yandex.ru>
2019-04-27 13:57:24 +0300

    updated

--- backend_layer/imgproc/imgproc.py
+++ backend_layer/imgproc/imgproc.py
@@ -3,6 +3,14 @@ import json
 import sys
 sys.path.append('../..') 
 
+sets = {
+    "max_width":  1500,
+    "max_height": 1500,    
+    "style": "magnificent",
+    "angle_tolerance": 0,
+    "position":"center"
+}
+
 def svg_to_paths(file_name):
     p=subprocess.call(['/usr/bin/inkscape','{file_path} --verb EditSelectAll --verb SelectionUngroup --verb ObjectToPath --verb FileQuit'])
 
@@ -13,4 +21,6 @@ def svg_to_points(settings: dict):
     
     with open('svg_to_points/svg_to_points.linux-arm64/data/result.json','w') as file:
         print(json.load(file))
-    
+
+if __name__ == "__main__":
+    svg_to_points(sets)
