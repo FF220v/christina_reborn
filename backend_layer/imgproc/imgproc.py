af93c02ca2f0f1c2314f21736e471be11143875d
FF220v <San987873@yandex.ru>
2019-04-27 14:57:14 +0300

    updated

--- backend_layer/imgproc/imgproc.py
+++ backend_layer/imgproc/imgproc.py
@@ -1,24 +1,27 @@
 import subprocess
 import json
 import sys
+import os
+import shutil
 sys.path.append('../..') 
 from common.logger import Logger
-"""
+os.chdir('../..')
 settings_example = {
     "max_width":  1500,
     "max_height": 1500,    
     "style": "magnificent",
     "angle_tolerance": 0,
-    "position":"center"
-    "make_paths":False
-    "path":"./file.svg"
+    "position":"center",
+    "make_paths":False,
+    "path":"backend_layer/imgproc/test.svg"
 }
-"""
+
 
 validation_list = ['max_width','max_height','style','angle_tolerance','position','make_paths','path']
 
-def svg_to_paths(file_name):
-    p=subprocess.call(['/usr/bin/inkscape','{file_path} --verb EditSelectAll --verb SelectionUngroup --verb ObjectToPath --verb FileQuit'])
+def svg_to_paths(file_path):
+    log.info('Converting all contours to paths...')
+    p=subprocess.call(['/usr/bin/inkscape','{file_path} --verb EditSelectAll --verb SelectionUngroup --verb ObjectToPath --verb FileQuit'])    
 
 def svg_to_points(settings: dict):
     log = get_logger_by_name(__name__)
@@ -28,15 +31,20 @@ def svg_to_points(settings: dict):
         if key not in settings:
             valid = False
             log.error('key {key} should be in settings'.format(key = key))
-    if valid:    
-        with open('svg_to_points/svg_to_points.linux-arm64/data/settings.json','w') as file:
+    if valid:
+        shutil.copy2(os.getcwd() + settings['path'], os.getcwd() + 'backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg') 
+        if settings['make_paths']:
+            svg_to_paths('backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg')     
+
+        with open('backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/settings.json','w') as file:
             json.dump(settings, file)
-        p=subprocess.call(['bash', 'svg_to_points/svg_to_points.linux-arm64/svg_to_points'])
+        p=subprocess.call(['bash', 'backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/svg_to_points'])
         
-        with open('svg_to_points/svg_to_points.linux-arm64/data/result.json') as file:
+        with open('backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/result.json') as file:
             print(json.load(file))
-        return 0
+        return 'backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/result.json'
     else:
-        return -1
+        log.error('Invalid settings. Cannot perform operation')
+
 if __name__ == "__main__":
-    svg_to_points(sets)
+    svg_to_points(settings_example)
