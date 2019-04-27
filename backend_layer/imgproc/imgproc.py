67402df5fa281905a1417f58755eb43f41225240
FF220v <san987873@yandex.ru>
2019-04-27 16:37:10 +0300

    Imgproc module can get settings and when convert svg files to points and save them near original ones

--- backend_layer/imgproc/imgproc.py
+++ backend_layer/imgproc/imgproc.py
@@ -2,29 +2,35 @@ import subprocess
 import json
 import sys
 import os
+sys.path.append('../..')
 import shutil
-sys.path.append('../..') 
 from common.logger import Logger
-os.chdir('../..')
+INKSCAPE_TIMEOUT = 20
 settings_example = {
-    "max_width":  1500,
-    "max_height": 1500,    
+    "max_width":  100,
+    "max_height": 100,    
     "style": "magnificent",
     "angle_tolerance": 0,
     "position":"center",
-    "make_paths":False,
-    "path":"backend_layer/imgproc/test.svg"
+    "make_paths":True,
+    "path":"/backend_layer/imgproc/test.svg"
 }
 
+log = Logger.get_logger_by_name(__name__)
+os.chdir('../..')
 
 validation_list = ['max_width','max_height','style','angle_tolerance','position','make_paths','path']
 
 def svg_to_paths(file_path):
     log.info('Converting all contours to paths...')
-    p=subprocess.call(['/usr/bin/inkscape','{file_path} --verb EditSelectAll --verb SelectionUngroup --verb ObjectToPath --verb FileQuit'])    
+    try:
+        log.info('Calling inkscape...')
+        p=subprocess.call('inkscape -f ' + os.getcwd() + file_path + ' --verb EditSelectAll --verb SelectionUnGroup --verb EditSelectAll --verb ObjectToPath --verb FileSave --verb FileQuit', shell = True, timeout = INKSCAPE_TIMEOUT)    
+        log.info('Points converted to paths')
+    except: 
+        log.error('Timeout expired')    
 
 def svg_to_points(settings: dict):
-    log = get_logger_by_name(__name__)
     log.info('Validating settings...')
     valid = True
     for key in validation_list:
@@ -32,17 +38,20 @@ def svg_to_points(settings: dict):
             valid = False
             log.error('key {key} should be in settings'.format(key = key))
     if valid:
-        shutil.copy2(os.getcwd() + settings['path'], os.getcwd() + 'backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg') 
+        log.info('Settings valid')
+        log.info('Making work copy /backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg')
+        shutil.copy2(os.getcwd() + settings['path'], os.getcwd() + '/backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg') 
         if settings['make_paths']:
-            svg_to_paths('backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg')     
+            svg_to_paths('/backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/file_to_convert.svg')     
 
         with open('backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/settings.json','w') as file:
             json.dump(settings, file)
+        log.info('Calling java app svg_to_points...')
         p=subprocess.call(['bash', 'backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/svg_to_points'])
         
         with open('backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/result.json') as file:
             print(json.load(file))
-        return 'backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/result.json'
+        shutil.copy2(os.getcwd() + '/backend_layer/imgproc/svg_to_points/svg_to_points.linux-arm64/data/result.json',os.getcwd() + settings['path'][:len(settings['path'])-3]+'json')
     else:
         log.error('Invalid settings. Cannot perform operation')
 
